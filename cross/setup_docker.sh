5d90bc231d61f64d9b76c857d05c8859606b09aa
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 19:05:35 +0800

    fix install error

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'apt-get install -y default-jdk && cd /workdir && /workdir/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'mkdir -p /usr/share/man/man1 && apt-get install -y default-jdk && cd /workdir && /workdir/cross/build.sh'
