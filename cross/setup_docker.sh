0427a59e69fdd80fe92b5d2646ec7e9d7ba4a442
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 18:59:33 +0800

    add jdk

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'apt-get install default-jdk && cd /workdir && /workdir/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'apt-get install -y default-jdk && cd /workdir && /workdir/cross/build.sh'
