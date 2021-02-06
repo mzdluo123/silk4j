788a1e527ddb48354aedd1eb12d77984ff4ef221
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 16:13:34 +0800

    change cacheDir

--- cross/build.sh
+++ cross/build.sh
@@ -1,5 +1,5 @@
 cd native
 mkdir cmake-build-debug
 cd cmake-build-debug
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=native/cmake-build-debug ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=. ..
 make install -j8
