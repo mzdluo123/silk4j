30df9922cdc7d5524884459e10a0b959076a26f9
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 19:32:24 +0800

    fix JAVA_HOME

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'mkdir -p /usr/share/man/man1 && apt-get install -y default-jdk && export JAVA_HOME=/usr/lib/jvm/default-java && cd /workdir && /workdir/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/workdir -v $JAVA_HOME:/java_home dockcross/$CROSS bash -c 'export JAVA_HOME=/java_home && cd /workdir && /workdir/cross/build.sh'
