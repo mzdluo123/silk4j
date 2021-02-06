956e9a1f5e4e7e6a959beb10b26f32cf88096cb5
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 20:52:12 +0800

    change image

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c '/work/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/work dockcross/$CROSS bash -c '/work/cross/build.sh'
