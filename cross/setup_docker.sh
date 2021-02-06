25b9934b0ec351d88b340b2d8465c56483f6c34a
HelloWorld <mzdluo123@foxmail.com>
2021-02-06 16:42:58 +0800

    Update setup_docker.sh

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS "cd /workdir && /workdir/cross/build.sh"
+docker run --rm -v $GITHUB_WORKSPACE:/workdir dockcross/$CROSS bash -c 'cd /workdir && /workdir/cross/build.sh'
