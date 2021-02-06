10ab5c1c3dcca0840bb20cbeb65fb912c8c5d664
mzdluo123 <mzdluo123@foxmail.com>
2021-02-06 21:12:56 +0800

    change name

--- cross/build.sh
+++ cross/build.sh
@@ -2,5 +2,5 @@ cd native
 mkdir cmake-build-debug
 cd cmake-build-debug
 rm -rf CMakeCache.txt
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=$PWD ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CACHEFILE_DIR=$PWD -Dcross_triple=$CROSS ..
 make install -j8
