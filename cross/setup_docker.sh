82465e7bf188bb6864e8135830d3c24c0ea655bd
mzdluo123 <mzdluo123@foxmail.com>
2021-02-07 19:34:41 +0800

    all in one jar

--- cross/setup_docker.sh
+++ cross/setup_docker.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $GITHUB_WORKSPACE:/work dockcross/$CROSS bash -c '/work/cross/build.sh'
+docker run --rm -v $GITHUB_WORKSPACE:/work --env CROSS=$CROSS dockcross/$CROSS bash -c '/work/cross/build.sh'
